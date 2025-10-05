39a930742666c6f60835251cf5e0f436e7b2d8e7
rivka barashi <r0583252581@gmail.com>
2025-10-05 19:26:32 +0300

    Props

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonDetails from './components/PersonDetails';
+
+function App() {
+  return (
+    <>
+    <PersonDetails/>
+   
+    </>
+  );
+}
+
+export default App;
---
+++ components/CourseDetails.jsx
@@ -0,0 +1,11 @@
+
+function CourseDetails({ courseName, grade }) {
+  return (
+    <>
+      <h4>{courseName}</h4>
+      <p>ציון: {grade}</p>
+  </>
+  );
+}
+
+export default CourseDetails;
